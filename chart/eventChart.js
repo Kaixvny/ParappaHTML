c01d6425dc6d455957a72118eadcfc644dc3f69f
Kai <68577140+Kaixvny@users.noreply.github.com>
2024-10-23 10:57:49 -0400

    Initial Commit of main index and literally all files

---
+++ chart/eventChart.js
@@ -0,0 +1,64 @@
+const sectionEvents = {
+    //Events are read like this... [ section: function() { what you want to happen in that section }]
+    40: function() { 
+        endBeat = 16; 
+    },
+    22: function() { 
+        leftSound = "jump1"; 
+    },
+    26: function() { 
+        rightSound = "pose2"; 
+    },
+    32: function() { 
+        leftSound = "duck1"; 
+        rightSound = "turn1"; 
+    },
+    41: function() { 
+        kickSound = 2; 
+        punchSound = 3; 
+    },
+    43: function() { 
+        blockSound = 2; 
+        leftSound = "duck2"; 
+        rightSound = "turn2"; 
+    },
+    49: function() {
+        if (currentIndex === 5) { 
+            leftSound = "jump2"; 
+            updateNoteSounds(); 
+        }
+    },
+    51: function() { 
+        if (currentIndex === 5) { 
+            rightSound = "pose1"; 
+            updateNoteSounds(); 
+        }
+    },
+    52: function() { //weird
+        leftSound = "duck2"; 
+        rightSound = "turn2"; 
+    },
+    54: function() { //also weird
+        leftSound = "jump2"; 
+        rightSound = "pose1"; 
+    },
+    62: function() { 
+        kickSound = 3; 
+        punchSound = 2; 
+        blockSound = 3; 
+        chopSound = 2; 
+    },
+    65: function() { //also weird??
+        kickSound = 2; 
+        rightSound = "turn1"; 
+    },
+    68: function() { 
+        leftSound = "duck2"; 
+    },
+    72: function() { 
+        leftSound = "jump2"; 
+        kickSound = 3; 
+        chopSound = 3; 
+        punchSound = 2; 
+    }
+};
---
+++ chart/noteChart.js
@@ -0,0 +1,140 @@
+// Song 1 Chart
+const noteChart = {
+    //chart entrys work like this [ "section-beat": "note" ]
+    //lesson 1
+    //section0
+    "0-2": "triangle",
+    "1-2": "triangle",
+    //section1
+    "2-2": "circle",
+    "3-2": "circle",
+    //section2
+    "4-2": "x",
+    "5-2": "x",
+    //section3
+    "6-2": "square",
+    "7-2": "square",
+    //section4
+    "8-2": "triangle",
+    "9-2": "triangle",
+    //section5
+    "10-2": "circle",
+    "11-2": "circle",
+    //section6
+    "12-2": "x",
+    "13-2": "x",
+    //section7
+    "14-2": "square",
+    "15-2": "square",
+
+    //lesson2
+    "20-2": "left",
+    "21-2": "left",
+    "22-2": "left",
+    "23-2": "left",
+    "24-2": "right",
+    "25-2": "right",
+    "26-2": "right",
+    "27-2": "right",
+    "28-2": "left",
+    "29-2": "left",
+    "30-2": "right",
+    "31-2": "right",
+    "32-2": "left",
+    "33-2": "left",
+    "34-2": "right",
+    "35-2": "right",
+
+    //lesson3
+    //sec1
+    "40-2": "triangle",
+    "40-6": "circle",
+    "41-2": "triangle",
+    "41-6": "circle",
+    //sec2
+    "42-2": "x",
+    "42-6": "square",
+    "43-2": "x",
+    "43-6": "square",
+    //sec3
+    "44-2": "x",
+    "44-6": "triangle",
+    "45-2": "x",
+    "45-6": "triangle",
+    //sec4
+    "46-2": "circle",
+    "46-6": "square",
+    "47-2": "circle",
+    "47-6": "square",
+    //sec5
+    "48-2": "left",
+    "48-6": "left",
+    "49-2": "left",
+    "49-6": "left",
+    //sec6
+    "50-2": "right",
+    "50-6": "right",
+    "51-2": "right",
+    "51-6": "right",
+    //sec7
+    "52-2": "left",
+    "52-6": "right",
+    "53-2": "left",
+    "53-6": "right",
+    //sec8
+    "54-2": "left",
+    "54-6": "right",
+    "55-2": "left",
+    "55-6": "right",
+
+    //lesson 3
+    //sec1
+    "61-2": "triangle",
+    "61-4": "circle",
+    "61-6": "square",
+    "62-2": "triangle",
+    "62-4": "circle",
+    "62-6": "square",
+    //sec 2
+    "63-2": "x",
+    "63-4": "triangle",
+    "63-6": "square",
+    "64-2": "x",
+    "64-4": "triangle",
+    "64-6": "square",
+    //sec 3
+    "65-2": "square",
+    "65-4": "right",
+    "65-6": "triangle",
+    "66-2": "square",
+    "66-4": "right",
+    "66-6": "triangle",
+    //sec 4
+    "67-2": "square",
+    "67-4": "left",
+    "67-6": "circle",
+    "68-2": "square",
+    "68-4": "left",
+    "68-6": "circle",
+    //sec 5
+    "69-2": "left",
+    "69-4": "left",
+    "69-6": "right",
+    "70-2": "left",
+    "70-4": "left",
+    "70-6": "right",
+    //sec 6
+    "71-2": "left",
+    "71-4": "triangle",
+    "71-6": "x",
+    "72-2": "left",
+    "72-4": "triangle",
+    "72-6": "x",
+    //sec7
+    "73-2": "circle",
+    "73-4": "circle",
+    "73-6": "circle",
+    "74-2": "circle",
+    "74-4": "circle",
+    "74-6": "circle"
+};
